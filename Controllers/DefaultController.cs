ba1fac260a0f2e4f692e516a65a1a090ec1920ae
mustafalmazz <mustafagfb19077091@gmail.com>
2025-06-25 03:15:22 +0300

    Vitrin Paneli Listeleme İşlemleri
    PartialView alanları için Listeleme işlemleri yapıldı , İletişime mesaj gönderme bölümü eklendi

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCvEntities db = new DbCvEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimler.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifika = db.TblSertifikalarim.ToList();
+            return PartialView(sertifika);
+        }
+        public PartialViewResult İletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult İletisim(Tbliletisim iletisim)
+        {
+            iletisim.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(iletisim);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
