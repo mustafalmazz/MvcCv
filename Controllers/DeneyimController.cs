034c0042d95f3ea1e621312a11a9b7413c9cf1c8
mustafalmazz <mustafagfb19077091@gmail.com>
2025-06-26 21:29:07 +0300

    Repository Design Pattern
    birtakım CRUD  işlemleri ve repository design pattern

---
+++ Controllers/DeneyimController.cs
@@ -0,0 +1,53 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+        DeneyimRepository repo = new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var list = repo.List();
+            return View(list);
+        }
+        public ActionResult DeneyimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DeneyimEkle(TblDeneyimler deneyim)
+        {
+            repo.TAdd(deneyim);
+            return RedirectToAction(nameof(Index));
+        }
+        public ActionResult DeneyimSil(int id)
+        {
+            TblDeneyimler t = repo.Find(a => a.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction(nameof(Index));
+        }
+        public ActionResult DeneyimGetir(int id)
+        {
+            TblDeneyimler t = repo.Find(a => a.ID == id);
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult DeneyimGetir(TblDeneyimler deneyim)
+        {
+            TblDeneyimler t = repo.Find(z=>z.ID == deneyim.ID);
+            t.Baslik = deneyim.Baslik;
+            t.AltBaslik = deneyim.AltBaslik;
+            t.Tarih = deneyim.Tarih;
+            t.Aciklama = deneyim.Aciklama;
+            repo.TUpdate(t);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Controllers/YetenekController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using MvcCv.Repositories;
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        YetenekRepository repo = new YetenekRepository();
+        // GET: Yetenek
+        public ActionResult Index()
+        {
+            var listem = repo.List();
+            return View(listem);
+        }
+    }
+}
