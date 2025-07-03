536365e8bbb41bddad84be550b8efa9acbfb0968
mustafalmazz <mustafagfb19077091@gmail.com>
2025-07-03 15:24:51 +0300

    Validation işlemleri
    validation işlemleri ve egitimler için CRUD işlemleri

--- Controllers/EgitimController.cs
+++ Controllers/EgitimController.cs
@@ -1,4 +1,6 @@
-﻿using MvcCv.Repositories;
+﻿using Microsoft.Ajax.Utilities;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,43 @@ namespace MvcCv.Controllers
         public ActionResult EgitimEkle()
         {
             return View();
+        }
+        [HttpPost] 
+        public ActionResult EgitimEkle(TblEgitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var egitim = repo.Find(x => x.ID == p.ID);
+            egitim.Baslik = p.Baslik;
+            egitim.AltBaslik1 = p.AltBaslik1;
+            egitim.AltBaslik2 = p.AltBaslik2;
+            egitim.Tarih = p.Tarih;
+            egitim.GNO = p.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
         }
     }
 }
