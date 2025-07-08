9c01408e275f7a27efc392e44cfab6ca48e32b21
mustafalmazz <mustafagfb19077091@gmail.com>
2025-07-08 17:54:52 +0300

    Sertifika CRUD işlemleri

--- Controllers/SertifikaController.cs
+++ Controllers/SertifikaController.cs
@@ -17,5 +17,36 @@ namespace MvcCv.Controllers
             var srtf = repo.List();
             return View(srtf);
         }
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika = repo.Find(z=>z.ID == id);
+            ViewBag.d = id;
+            return View(sertifika);
+        }
+        [HttpPost]
+        public ActionResult SertifikaGetir(TblSertifikalarim p)
+        {
+            var sertifika = repo.Find(m=>m.ID == p.ID);
+            sertifika.Aciklama = p.Aciklama;
+            sertifika.Tarih = p.Tarih;
+            repo.TUpdate(sertifika);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YeniSertifika()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSertifika(TblSertifikalarim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika = repo.Find(a=>a.ID == id);
+            repo.TDelete(sertifika);
+            return RedirectToAction("Index");
+        }
     }
 }
