43c20e53d3cb1c2a5df8be02338f00d7c2449c3e
mustafalmazz <mustafagfb19077091@gmail.com>
2025-07-10 11:34:47 +0300

    iletişm ve hobilerim sayfası

---
+++ Controllers/iletisimController.cs
@@ -0,0 +1,21 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class iletisimController : Controller
+    {
+        GenericRepository<Tbliletisim> repo = new GenericRepository<Tbliletisim>();
+        // GET: iletisim
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
